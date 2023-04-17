42c6ce8067c2938b66dcbb3f347e0c4caa8afa0b
Stella Inwood <hello@koding.dev>
2023-04-18 00:43:37 +1000

    feat: add hanako art

---
+++ src/lib/data/blog/nothin/meta.ts
@@ -0,0 +1,7 @@
+import type { PostMetadata } from '$lib/data/blog';
+import cover from './images/pussy.png?image';
+
+// noinspection JSUnusedGlobalSymbols
+export default {
+  cover,
+} as Partial<PostMetadata>;
